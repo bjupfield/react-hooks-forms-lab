52bca21e883cbe8a1679b39e22202fec3c71359f
Bennett <bjupfield@gmail.com>
2021-11-24 12:54:41 -0600

    test doesnt say complete because i made the updateing of the item list create the object to add within itself, dont feel like changing...

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { v4 as uuid } from "uuid";
+function ItemForm({onSelectCat, itemName, onItemName, onItemFormSubmit}) {
 
-function ItemForm(props) {
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit = {onItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value = {itemName} onChange = {onItemName}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange = {onSelectCat}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,24 +2,49 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, changedatText] = useState("");
+  const [selectCat, setSelectCat] = useState("")
+  const [itemName, setItemName] = useState("")
+  const [itemarr, setItemArr] = useState([...items]);
 
+  function onSelectCat(e){
+    setSelectCat(e.target.value)
+  }
+  function onItemName(e){
+    setItemName(e.target.value)
+  }
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function onSearchChange(e){
+    changedatText(e.target.value)
+  }
+  function onItemFormSubmit(e){
+    e.preventDefault()
+    setItemArr([...itemarr, {
+      id: uuid(),
+      name: itemName,
+      category: selectCat,
+    }])
+  }
+
+  const itemsToDisplay = itemarr.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+  }).filter((item)=>{
+    return item.name.includes(search)
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemName = {onItemName} itemName = {itemName} onSelectCat = {onSelectCat} onItemFormSubmit = {onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} search = {search} onSearchChange = {onSearchChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
